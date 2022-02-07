37ce05a0574c9ac34733483e7d80c945f551b49c
ijazmohamedumar <71162022+ijazmohamedumar@users.noreply.github.com>
2022-02-06 19:23:51 -0600

    added ForgotPassword Page

--- src/ui/auth/ForgotPassword.tsx
+++ src/ui/auth/ForgotPassword.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import {useState } from 'react';
+import './ForgotPassword.css';
+import Navbar from './../Navbar';
 
 export default function ForgotPassword() 
 {
+    const [email, setEmail] = useState('');
+    const handleSubmit = (e: any) => 
+    {
+        e.preventDefault();
+        ForgotPassword();
+    };
+
+    async function ForgotPassword() 
+    {
+        console.log(email);
+    }
   return(
     <div>
-        <h1>Forgot Password</h1>
+      <Navbar />
+      <div className="forgotPassPage">
+        <form className = "myForm" onSubmit={handleSubmit}>
+          <div className="formBox">
+            <h2>PASSWORD RESET</h2>
+            <div className="formInfo">
+              <label>
+                Email:
+                <input
+                  className="email"
+                  type="email"
+                  placeholder="example@gmail.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
+            </div>
+            <input type="submit" value="SUBMIT" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
