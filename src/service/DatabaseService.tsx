051358cd7660ccdee761c6e6dc470c5a25448fae
getBoolean <danielwnewville@gmail.com>
2022-02-26 16:22:13 -0600

    Use allCoursesConverter for getAllCoursesObject

--- src/service/DatabaseService.tsx
+++ src/service/DatabaseService.tsx
@@ -69,7 +69,59 @@ const userDataConverter = {
   }
 };
 
+const allCoursesConverter = {
+  toFirestore(course: Course): DocumentData {
+    return {
+      firebaseId: course.firebaseId,
+      courseId: course.courseId,
+      coreqIds: course.coreqIds,
+      prereqIds: course.prereqIds,
+      courseNumber: course.courseNumber,
+      department: course.department,
+      description: course.description,
+      requiredInMajors: course.requiredInMajors,
+      availability: course.availability,
+    };
+  },
+  fromFirestore(
+    snapshot: QueryDocumentSnapshot,
+    options: SnapshotOptions
+  ): Course {
+    const data = snapshot.data(options)!;
+
+    const availabilityMap = data.availability;
+    const availability = new Availability(
+      availabilityMap.fall,
+      availabilityMap.spring,
+      availabilityMap.summer,
+    );
+    const requiredInMajors: Major[] = [];
+    
+    for (let i = 0; i < data.requiredInMajors.length; i++) {
+      const major = majorEnumFromString(data.requiredInMajors[i]);
+      if (major !== undefined) {
+        requiredInMajors.push(major);
+      }
+    }
+
+    const course = new Course(
+      snapshot.id,
+      data.coreqIds,
+      data.prereqIds,
+      data.courseId,
+      data.courseNumber,
+      data.department,
+      data.description,
+      requiredInMajors,
+      availability,
+    );
+
+    return course;
+  }
+};
+
 const userDataDocument = (userId: string) => doc(db, 'users', userId).withConverter(userDataConverter);
+const allCoursesCollection = collection(db, 'allCourses').withConverter(allCoursesConverter);
 
 /**
  * @deprecated Since version Feb 20, 2022. Will be deleted soon. Use `getAllCoursesObject` instead.
@@ -90,45 +142,15 @@ const getAllCourses = async (): Promise<DocumentData[] | null> => {
  * @returns Courses
  */
 const getAllCoursesObject = async (): Promise<Course[] | null> => {
-  const allCoursesCol = collection(db, 'allCourses');
-  const courses : Course[] | null = await getDocs(allCoursesCol)
-    .then((snapshot: QuerySnapshot<DocumentData>) => {
-      let documents: DocumentData[] = [];
+  const courses : Course[] | null = await getDocs(allCoursesCollection)
+    .then((snapshot: QuerySnapshot<Course>) => {
       let courses: Course[] = [];
       for (let i = 0; i < snapshot.docs.length; i++) {
-        const documentSnapshot: QueryDocumentSnapshot<DocumentData> | undefined = snapshot.docs.at(i);
+        const documentSnapshot: QueryDocumentSnapshot<Course> | undefined = snapshot.docs.at(i);
         if (documentSnapshot !== undefined && documentSnapshot.exists()) {
           const data = documentSnapshot.data();
-          documents.push(data);
-
-          const availabilityMap = data.availability;
-          const availability = new Availability(
-            availabilityMap.fall,
-            availabilityMap.spring,
-            availabilityMap.summer,
-          );
-          const requiredInMajors: Major[] = [];
           
-          for (let i = 0; i < data.requiredInMajors.length; i++) {
-            const major = majorEnumFromString(data.requiredInMajors[i]);
-            if (major !== undefined) {
-              requiredInMajors.push(major);
-            }
-          }
-
-          const course = new Course(
-            documentSnapshot.id,
-            data.coreqIds,
-            data.prereqIds,
-            data.courseId,
-            data.courseNumber,
-            data.department,
-            data.description,
-            requiredInMajors,
-            availability,
-          );
-
-          courses.push(course);
+          courses.push(data);
         }
       }
       return courses;
